0d4d721d475e3dd94b18042aeaf1c8c75de70e08
johns-children <taotan421@gmail.com>
2024-01-28 00:21:26 +0900

    Use tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <h1>Vite + Vue.js + tailwindCSS</h1>
-    <div>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+  <div class="flex h-lvh">
+    <div class="w-40 p-5 bg-gray-100">
+      <h1 class="mb-3 text-lg font-bold text-blue-600">Vite + Vue.js + tailwindCSS</h1>
+      <router-link to="/" class="block mb-3 no-underline hover:underline">Home</router-link>
+      <router-link to="/about" class="block mb-3 no-underline hover:underline">About</router-link>
     </div>
 
-    <router-view class="main flex-item"/>
+    <router-view class="p-5"/>
   </div>
 </template>
 
