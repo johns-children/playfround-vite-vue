d2bd69f6598fe61839e9748d0edfa5fb3cbffc5d
johns-children <taotan421@gmail.com>
2024-01-27 23:55:43 +0900

    Set up tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app">
-    <div id="sidebar">
+  <div>
+    <h1>Vite + Vue.js + tailwindCSS</h1>
+    <div>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
@@ -14,35 +15,3 @@ export default {
   name: 'App',
 };
 </script>
-
-<style>
-#app {
-  height: 100vh;
-  display: flex;
-}
-
-#sidebar {
-  width: 100px;
-  padding: 20px;
-  background-color: #f0f0f0;
-}
-
-#sidebar a {
-  display: block;
-  margin-bottom: 10px;
-  color: #333;
-  text-decoration: none;
-}
-
-#sidebar a:hover {
-  text-decoration: underline;
-}
-
-.main {
-  padding: 20px
-}
-
-.flex-item {
-  flex: 1
-}
-</style>
